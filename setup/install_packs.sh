4e0d279c169457ffcb226e5eff238eb8ae4752fd
hfbassani <hfb@cin.ufpe.br>
2024-04-03 17:29:44 -0400

    - Update install_packs.sh to create the virtual enviroment. - Update README.md with a step to run this script for setup.

--- setup/install_packs.sh
+++ setup/install_packs.sh
@@ -5,3 +5,8 @@ brew install portaudio
 # debian based:
 sudo apt install python-dev
 sudo apt install portaudio19-dev
+
+# Create venv (requires python3.10 or later):
+python3.10 -m venv ./venv_cozmini
+source ./venv_cozmini/bin/activate
+pip install -r requirements.txt

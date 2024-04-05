6a8020962d357c71916e4e71a35179cc5eefb824
Hansenclever Bassani <hfb@cin.ufpe.br>
2024-04-05 10:37:33 -0400

    Update install_packs.sh
    Check the OS and perform the OS-specific steps.

--- setup/install_packs.sh
+++ setup/install_packs.sh
@@ -1,12 +1,21 @@
-# MacOS:
-brew install ffmpeg
-brew install portaudio
+#!/usr/bin/env bash
 
-# debian based:
-sudo apt install python-dev
-sudo apt install portaudio19-dev
-
-# Create venv (requires python3.10 or later):
-python3.10 -m venv ./venv_cozmini
-source ./venv_cozmini/bin/activate
-pip install -r requirements.txt
+if [ "$(uname)" == "Darwin" ]; then
+  # MacOS:
+  brew install ffmpeg
+  brew install portaudio
+  # Create venv (requires python3.10 or later):
+  python -m venv ./venv_cozmini
+  source ./venv_cozmini/bin/activate
+  pip install -r requirements.txt
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  # debian based only:
+  sudo apt install python3.10-dev
+  sudo apt install portaudio19-dev
+  # Create venv (requires python3.10 or later):
+  python3.10 -m venv ./venv_cozmini
+  source ./venv_cozmini/bin/activate
+  pip install -r requirements.txt
+else
+  echo "Sorry, this operating system is not supported yet." 
+fi

b69a134f276d6b9a55f788886c370fb52710b35a
Hansenclever Bassani <hfb@cin.ufpe.br>
2024-04-05 10:25:29 -0400

    Create default (example) env_keys.sh

---
+++ keys/env_keys.sh
@@ -0,0 +1,3 @@
+export PICOVOICE_KEYWORD_PATH=./keys/[enter your hey-cozmo*.ppn here]
+export PICOVOICE_ACCESS_KEY=[enter your Picovoice key here]
+export GOOGLE_API_KEY=[enter your google API key here]

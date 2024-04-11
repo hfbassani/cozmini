3fbd03a7a7db50b29d655a522dac089085a3d337
Hans Bassani <hfb@cin.ufpe.br>
2024-04-11 12:14:22 -0400

    - Fix set init in cozmini.py - use python3 on macos install

--- setup/set_env.sh
+++ setup/set_env.sh
@@ -1,5 +1,3 @@
-export PICOVOICE_KEYWORD_PATH=./keys/hey-cosmo_en_linux_v3_0_0.ppn
-
 # ADB path is required for android devices if adb is not in the PATH
-# ADB_PATH=/Users/hans/Documents/projects/platform-tools
-# export PATH=".:$ADB_PATH:$PATH"
+ADB_PATH=/Users/hans/Documents/projects/platform-tools
+export PATH=".:$ADB_PATH:$PATH"

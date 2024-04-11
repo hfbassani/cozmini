e3b6380dbc76543a4170abcac661bbd6bb79ac2a
Hans Bassani <hfb@cin.ufpe.br>
2024-04-11 12:17:57 -0400

    Create a default version for the set_env.sh file

--- setup/set_env.sh
+++ setup/set_env.sh
@@ -1,3 +1,5 @@
 # ADB path is required for android devices if adb is not in the PATH
-ADB_PATH=/Users/hans/Documents/projects/platform-tools
-export PATH=".:$ADB_PATH:$PATH"
+# Enter the path for the ADB platform-tools and uncomment de lines below.
+
+# ADB_PATH=[ENTER your ADB platform-tools path here]
+# export PATH=".:$ADB_PATH:$PATH"

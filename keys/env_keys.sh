a30111ad3b6505ed1486a56204232ce322b99b37
Hansenclever Bassani <hfb@cin.ufpe.br>
2024-06-09 17:44:40 -0400

    Add comments to env_keys.sh

--- keys/env_keys.sh
+++ keys/env_keys.sh
@@ -1,3 +1,6 @@
+# If you change this file, to prevent sending keys to the repository run:
+# git update-index --assume-unchanged keys/env_keys.sh
+
 export PICOVOICE_KEYWORD_PATH=./keys/[enter your hey-cozmo*.ppn here]
 export PICOVOICE_ACCESS_KEY=[enter your Picovoice key here]
 export GOOGLE_API_KEY=[enter your google API key here]
